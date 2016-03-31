af58bb27c495483bc8426d53d5cec3824644257a
rueda19 <ruedabobadilla@gmail.com>
2016-03-31 17:54:50 +0200

    55555

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace ConsoleApplication1
 {
@@ -11,7 +12,58 @@ namespace ConsoleApplication1
         {
             //System.Diagnostics.Debug.Write(Sys);
             String user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            String pass = System.Security.Principal.WindowsIdentity.GetCurrent().Groups.ToString();
             System.Diagnostics.Debug.Write(user);
+
+            /*
+            "Data Source=ServerName;" +
+            "Initial Catalog=DataBaseName;" +
+            "Integrated Security=SSPI;";
+             */
+            string connectionString =
+            "Data Source=svr008;Initial Catalog=Fw_GrupoGarnica;"
+            + "Integrated Security=true";
+
+            // Provide the query string with a parameter placeholder.
+            string queryString =
+                "SELECT [ID] ,[Usuario] ,[NombreCompleto] ,[Departamento] FROM [Fw_GrupoGarnica].[dbo].[GP_Empleado]";
+            /*"SELECT ProductID, UnitPrice, ProductName from dbo.products "
+                + "WHERE UnitPrice > @pricePoint "
+                + "ORDER BY UnitPrice DESC;";*/
+
+            // Specify the parameter value.
+            int paramValue = 5;
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //command.Parameters.AddWithValue("@pricePoint", paramValue);
+
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}",
+                            reader[0], reader[1], reader[2]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
