5705e30de805b0902aa64e7b1f141b3ed047ced4
rueda19 <ruedabobadilla@gmail.com>
2016-03-31 17:31:02 +0200

    444

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Diagnostics.Debug.Write(Sys);
+            String user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            System.Diagnostics.Debug.Write(user);
+        }
+    }
+}
